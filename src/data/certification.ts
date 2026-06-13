bea0412cb93c7bc5690043d57713b8d213f17fa4
NickDevs <nick022190@gmail.com>
2026-06-13 22:18:23 +0800

    added 2 items in certifications

--- src/data/certification.ts
+++ src/data/certification.ts
@@ -8,6 +8,24 @@ export interface Certification {
 
 export const certificationData: Certification[] = [
   {
+    title: "Design Prompts for Everyday Work Tasks",
+    issuer: "Google",
+    date: "June 2026",
+    shortDescription:
+      "",
+    credentialUrl: "https://coursera.org/share/f51cd03ad3fbf5e892e2d2e30179ff62",
+  },
+
+   {
+    title: "Start Writing Prompts like a Pro",
+    issuer: "Google",
+    date: "June 2026",
+    shortDescription:
+      "",
+    credentialUrl: "https://coursera.org/share/9012be5295c6cbb969ac755fa4059d6d",
+  },
+
+  {
     title: "Web Development",
     issuer: "SoloLearn",
     date: "July 2025",
@@ -24,4 +42,6 @@ export const certificationData: Certification[] = [
       "Completed the course by demonstrating theorictical and practical undestanding of Data Analytics with AI.",
     credentialUrl: "https://www.sololearn.com/certificates/CC-NT8WNV73",
   },
+
+  
 ];
