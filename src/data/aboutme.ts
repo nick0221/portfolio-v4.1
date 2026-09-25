15401ab1cd257a755d9a02bac64e9a45ba1dfff2
nick0221 <nick022190@gmail.com>
2026-09-25 06:35:59 -0400

    replace the cv pdf file to the latest

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -31,7 +31,7 @@ export const aboutMe: AboutMe = {
   linkedinUsername: "nickzworld",
   twitterUsername: "",
   // blogUrl: "https://",
-  cvUrl: "/cv/rsevilla-cv-fullstackdevs.pdf",
+  cvUrl: "/cv/RomnickSevilla-FULLSTACKWEBDEVELOPER.pdf",
   // institutionUrl: "https://www.stanford.edu",
   // altName: "Nick",
   // secretDescription: "I like dogs.",
